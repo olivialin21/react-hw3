35d86e6d4d03548169885e54fb9a56467adb8591
olivialin21 <s110819023@stu.ntue.edu.tw>
2021-03-14 10:36:34 +0800

    navbar add

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return (
+        <nav className="navbar">
+            <h2 className="text-white">Textile</h2>
+            <h2 className="text-white">Cookware</h2>
+            <h2 className="text-white">Home Accessories</h2>
+            <h2 className="text-white">Lighting</h2>
+            <h2 className="text-white">Tableware</h2>
+            <h2 className="text-white">Furniture</h2>
+        </nav>
+    );
+
+}
